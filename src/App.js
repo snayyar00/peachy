42fe1489f20563fc66edfd23916fb86a36ffc34f
snayyar00 <39316104+snayyar00@users.noreply.github.com>
2020-12-27 03:14:40 -0800

    sign in and input and buttons done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component'
+import SignInandSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInandSignUp} />
       </Switch>
     </div>
   );
