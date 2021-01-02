45a85d4b6e4fa82e2fd2686c5bd56b2f6838c7c9
snayyar00 <39316104+snayyar00@users.noreply.github.com>
2021-01-01 23:42:26 -0800

    Revert "css is js"
    This reverts commit e0a5de99c7971f3a3ba6b371e7290aa4f3f0c3b7.

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,10 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [logger];
 
 if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
+    middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
